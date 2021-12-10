c21e4ed0cb611e90ea223d6121c59bead613fc6e
HanneMaes <Maes.hanne@gmail.com>
2021-12-10 11:57:34 +0100

    Add files via upload

---
+++ Search Engine Shortcuts.user.js
@@ -0,0 +1,133 @@
+// ==UserScript==
+// @name         Search Engine Shortcuts
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.google.com/*
+// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('Search Engine Shortcuts: Google detected')
+
+    var searchResults = []
+
+    // add the styling to the page
+    document.head.innerHTML = document.head.innerHTML + `
+<link href="https://fonts.googleapis.com/css2?family=Nova+Mono&display=swap" rel="stylesheet">
+<style>
+  .googleSearchResultsShortcut, .googleSearchResultsShortcut::after {
+    border-radius: 5px;
+    border: 2px solid black;
+    cursor: default;
+  }
+
+  .googleSearchResultsShortcut {
+    /* positioning */
+    position: absolute;
+    right: 20px;
+    top: 0px;
+    /* size */
+    width: 20px;
+    height: 20px;
+
+    /* background */
+    background-color: white;
+    border-radius: 5px;
+    /* typography */
+    font-family: 'Nova Mono', monospace;
+    color: black;
+
+    /* center text */
+    text-align: center;
+    line-height: 19px;
+  }
+
+  .googleSearchResultsShortcut::after {
+    content: '';
+    display: block;
+
+    /* draw this underneath the div */
+    z-index: -1;
+
+    /* size */
+    width: 20px;
+    height: 20px;
+
+    /* positioning */
+    position: relative;
+    left: -2px;
+    top: -15px;
+
+    background-color: rgb(0, 0, 0);
+    box-shadow: 0 3px 14px rgba(0, 0, 0, 0.4);
+  }
+</style>
+`
+    //console.log("document.head.innerHTML:", document.head.innerHTML);
+
+    // get the search result HTML code so I can inject the shortcut icons
+    /*var results = document.getElementsByClassName("g")
+    console.log('results:', results)
+    Object.keys(results).forEach(function (item) {
+
+        searchResults.push({
+            html: results[item],
+            href: results[item].children[2],
+        })
+    })
+    console.log('searchResults:', searchResults)*/
+
+    // get the search result HREF so I can bind a shortcut to the link
+    var links = document.querySelectorAll('a');
+    for(var i = 0; i < links.length; i++){
+
+        // if it's not a link to google and not an empty link
+        if(!(links[i].href.includes('google')) && links[i].href != "") {
+
+            searchResults.push({
+            html: links[i],
+            href: links[i].href,
+        })
+        }
+    }
+    console.log('searchResults:', searchResults)
+
+    // get the search result HREF so I can bind a shortcut to the link
+    /*var i = 0
+    var results = document.getElementsByClassName("r")
+    Object.keys(results).forEach(function (item) {
+
+        searchResults[i].href = results[item].firstChild.href,
+            i++
+    })*/
+
+    // display the shortcut graphics
+    var i = 0
+    for (var i in searchResults) {
+        if (i<9) { // only display the first 9 items
+
+            searchResults[i].html.innerHTML = `
+      <div style="position: relative; width: 0; height: 0; overfow: visible;">
+        <div class="googleSearchResultsShortcut">` + parseInt(parseInt(i, 10) + 1, 10) + `</div>
+      </div>
+    ` + searchResults[i].html.innerHTML
+            //console.log('- searchResults[i].html.innerHTML:', searchResults[i].html.innerHTML)
+
+        }
+    }
+
+    // do this when a key is pressed
+    document.addEventListener('keypress', logKey);
+    function logKey(e) {
+        for (i = 1; i <= 9; i++) { // only display the first 9 items
+            console.log('go to:', searchResults[i-1].href)
+            //if (e.key == i+'') window.location.href = searchResults[i-1].href
+        }
+    }
+
+})();
